09c38ba74e9c85595dbab5425ede94f652160923
ankitkumardev25 <ankitkumar1999825@gmail.com>
2023-09-25 22:26:10 +0530

    problem 15

--- Medium/Problem 15- 3Sum/JS-sol.js
+++ Medium/Problem 15- 3Sum/JS-sol.js
@@ -8,20 +8,20 @@ var threeSum = function (nums) {
 
   for (let i = 0; i < nums.length - 2; i++) {
     if (i === 0 || nums[i] !== nums[i - 1]) {
-      let lo = i + 1,
-        hi = nums.length - 1,
+      let left = i + 1,
+        right = nums.length - 1,
         sum = 0 - nums[i]
-      while (lo < hi) {
-        if (nums[lo] + nums[hi] === sum) {
-          res.push([nums[i], nums[lo], nums[hi]])
-          while (lo < hi && nums[lo] === nums[lo + 1]) lo++
-          while (lo < hi && nums[hi] === nums[hi - 1]) hi--
-          lo++
-          hi--
-        } else if (nums[lo] + nums[hi] < sum) {
-          lo++
+      while (left < right) {
+        if (nums[left] + nums[right] === sum) {
+          res.push([nums[i], nums[left], nums[right]])
+          while (left < right && nums[left] === nums[left + 1]) left++
+          while (left < right && nums[right] === nums[right - 1]) right--
+          left++
+          right--
+        } else if (nums[left] + nums[right] < sum) {
+          left++
         } else {
-          hi--
+          right--
         }
       }
     }
